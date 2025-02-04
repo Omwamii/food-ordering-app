3697a32e252be2561880c257880f2acc742ee50f
omwami <ianalpha@protonmail.com>
2025-02-04 13:30:33 +0300

    added top navigators for order types

--- app/src/app/(admin)/orders/_layout.tsx
+++ app/src/app/(admin)/orders/_layout.tsx
@@ -7,7 +7,7 @@ import Colors from "@/constants/Colors";
 export default function MenuStack() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: 'Orders'}}/>
+            <Stack.Screen name="list" options={{ headerShown: false}}/>
         </Stack>
     )
 }
